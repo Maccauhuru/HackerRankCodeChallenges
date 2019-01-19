2921d723d56525d115a5e432b283a2022f909ffe
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2019-01-18 22:26:41 -0600

    Day 4 challenge

---
+++ Day4-Class-vs-Instance.js
@@ -0,0 +1,22 @@
+function Person(initialAge) {
+    // Add some more code to run some checks on initialAge
+    this.initialAge = initialAge;
+    if (this.initialAge < 0) {
+        this.initialAge = 0;
+        console.log("Age is not valid, setting age to 0.");
+    }
+    this.amIOld = function () {
+        // Do some computations in here and print out the correct statement to the console
+        if (this.initialAge < 13) {
+            console.log("You are young.")
+        } else if (this.initialAge >= 13 && this.initialAge < 18) {
+            console.log("You are a teenager.")
+        } else {
+            console.log("You are old.")
+        }
+    };
+    this.yearPasses = function () {
+        // Increment the age of the person in here
+        this.initialAge++;
+    };
+}
