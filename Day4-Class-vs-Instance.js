f709507dbdff1bce566c2ce9721ea66fe2b7f8bf
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2019-01-18 22:27:48 -0600

    Day 4 challenge

--- Day4-Class-vs-Instance.js
+++ Day4-Class-vs-Instance.js
@@ -1,3 +1,15 @@
+// Task
+// Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter.The constructor must assign to after confirming the argument passed as is not negative;
+// if a negative argument is passed as, the constructor should set to and print Age is not valid, setting age to 0..In addition, you must write the following instance methods:
+
+//     yearPasses() should increase the instance variable by.
+// amIOld() should perform the following conditional actions:
+//     If, print You are young..
+// If and, print You are a teenager..
+// Otherwise, print You are old..
+
+
+
 function Person(initialAge) {
     // Add some more code to run some checks on initialAge
     this.initialAge = initialAge;
